45e442eb471097b7468cfc2418c030d987b1ca86
gustavoleal-web <lealgustavo.web@gmail.com>
2020-11-26 14:41:34 -0600

    added styles to create a vetical layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Pokedex from './Pokedex'
+import styles from './App.module.css';
+
 
 //Pokedex
 //kanto 1 -151
@@ -16,11 +18,16 @@ const App = () => {
   const pekedexStart = {
     kanto: [ 0, 151 ],
     johto: [ 152, 250 ],
-    hoenn: [ 251, 386 ]
+    hoenn: [ 251, 386 ],
+    sinnoh: [386, 493],
+    unova: [494, 649],
+    kalos: [650, 721],
+    alola: [721, 809],
+    galar: [809, 898]
   }
 
   const fetchSelectedPokedex = ( e = 'kanto' ) => {
-    
+
     let cutPokedex = state.slice( pekedexStart[ e.target.name ][ 0 ], pekedexStart[ e.target.name ][ 1 ] )
     setNewPokedex( cutPokedex )
   }
@@ -43,15 +50,24 @@ const App = () => {
 
   return (
 
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.buttonContainer}>
         <button name='kanto' onClick={ fetchSelectedPokedex }>Kanto</button>
         <button name='johto' onClick={ fetchSelectedPokedex } >Johto</button>
         <button name='hoenn' onClick={ fetchSelectedPokedex } >Hoenn</button>
+        <button name='sinnoh' onClick={ fetchSelectedPokedex } >Sinnoh</button>
+        <button name='unova' onClick={ fetchSelectedPokedex } >Unova</button>
+        <button name='kalos' onClick={ fetchSelectedPokedex } >Kalos</button>
+        <button name='alola' onClick={ fetchSelectedPokedex } >Alola</button>
+        <button name='galar' onClick={ fetchSelectedPokedex } >Galar</button>
+      </div>
+      <div>
+        { state.length !== 0
+          ? newPokedex.map( pokemon => <Pokedex key={ pokemon.pokemon_species.url } id={ pokemon.entry_number } /> )
+          : <p>Loading</p> }
       </div>
 
 
-      {state.length !== 0 ? newPokedex.map( pokemon => <Pokedex key={ pokemon.pokemon_species.url } id={ pokemon.entry_number } /> ) : <p>Loading</p> }
     </div>
   )
 
