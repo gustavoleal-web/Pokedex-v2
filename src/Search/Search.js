9232e104e52ddb8c8d9a28c93f8c3c8f82f4639b
gustavoleal-web <lealgustavo.web@gmail.com>
2020-12-06 17:08:49 -0600

    passed down func from App to search when button is clicked

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,31 +1,28 @@
 import React, { useState } from 'react';
 import Pokedex from '../Pokedex/Pokedex'
 
+//{name.length !== 0
+// questionMark <Pokedex id={ name } />
+// : null }
+
+
 //TODO: clear input after button is pressed
 
-const Search = () => {
-    let tempName = '';
+const Search = ( { clicked, wasClicked } ) => {
     const [ name, setName ] = useState( '' );
 
     const updateName = ( e ) => {
-        tempName = e.target.value;
-
+        setName( e.target.value )
     }
 
-    const fetchSearchedPokemon = ( e ) => {
-        setName( tempName );
-    }
-    
     return (
         <>
             <input type="text" placeholder='bulbasaur' onChange={ updateName } />
-            <button onClick={ fetchSearchedPokemon }>Search</button>
-            {name.length !== 0
-                ? <Pokedex id={ name } />
-                : null
-            }
+            <button onClick={ () => wasClicked( name ) }>Search</button>
+
         </>
     )
 }
 
 export default Search;
+
