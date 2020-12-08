f742cc344ab50db352f27351cbfb0b9acface8fd
gustavoleal-web <lealgustavo.web@gmail.com>
2020-12-07 19:39:39 -0600

    fixed issued that would not load pokedex when the button was clicked

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Pokedex from './Pokedex/Pokedex'
 import SelectPokedex from './SelectedPokedex/SelectPokedex'
 import Search from './Search/Search';
 import styles from './App.module.css';
+//object[i].pokemon_species.name
 
+//TODO: might no even need to use clicked ans setClicked
+//TODO instead check to see if the string is empty. If it is don't do anthing. 
+//TODO: option 2: disable the button until user enters a valid pokemon name. 
 
 const App = () => {
-  const [ state, setState ] = useState( [] )
-  const [ newPokedex, setNewPokedex ] = useState( [] )
-  const [ clicked, setClicked ] = useState( false )
-  const [ pokemonName, setpokemonName ] = useState( '' )
+  const [ state, setState ] = useState( [] );
+  const [ newPokedex, setNewPokedex ] = useState( [] );
+  const [ clicked, setClicked ] = useState( false );
+  const [ pokemonName, setpokemonName ] = useState( '' );
+
 
 
   //set the start and end of each pokedex so on click event will slice from start to finish
@@ -26,19 +31,20 @@ const App = () => {
   }
 
   const fetchSelectedPokedex = ( e = 'kanto' ) => {
-
-    let cutPokedex = state.slice( pekedexStart[ e.target.name ][ 0 ], pekedexStart[ e.target.name ][ 1 ] )
-    setNewPokedex( cutPokedex )
+    setClicked( false );
+    let cutPokedex = state.slice( pekedexStart[ e.target.name ][ 0 ], pekedexStart[ e.target.name ][ 1 ] );
+    setNewPokedex( cutPokedex );
+    console.log( cutPokedex )
   }
 
   useEffect( () => {
 
     const fetchData = async () => {
       try {
-        let pokemon = await axios.get( ` https://pokeapi.co/api/v2/pokedex/national ` )
+        let pokemon = await axios.get( ` https://pokeapi.co/api/v2/pokedex/national ` );
         let pokemonEntries = pokemon.data.pokemon_entries;
-        setState( pokemonEntries )
-        setNewPokedex( pokemonEntries.slice( 0, 151 ) )
+        setState( pokemonEntries );
+        setNewPokedex( pokemonEntries.slice( 0, 151 ) );
       }
       catch ( e ) {
         console.log( e );
@@ -52,14 +58,21 @@ const App = () => {
       setClicked( false );
       return;
     }
-    else if(name.length >= 3 && name.length < 12){
+    else if ( name.length >= 3 && name.length < 12 ) {
       name = name.toLowerCase().trim();
       setpokemonName( name );
       setClicked( true );
     }
 
+    // for ( let i = 0; i < state.length; i++ ) {
+    //   if ( state[ i ].pokemon_species.name.includes( name ) ) {
+    //      setNewPokedex( pokemon );
+    //     break;
+    //   }
+    // }
+
   }
-  console.log( pokemonName )
+
   return (
 
     <div className={ styles.container }>
