c55f8973493f57bb285aad11d4c79f67fe0ed17a
gustavoleal-web <lealgustavo.web@gmail.com>
2020-12-09 14:45:33 -0600

    added styles for btn to turn grn when input meets passing conditions

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,32 +1,35 @@
 import React, { useState } from 'react';
-
-//{name.length !== 0
-// questionMark <Pokedex id={ name } />
-// : null }
-
+import styles from './search.module.css';
 
 //TODO: clear input after button is pressed
 
-const Search = ( { clicked, wasClicked } ) => {
+const Search = ( { wasClicked } ) => {
     const [ name, setName ] = useState( '' )
     const [ isDisbled, setIsDisabled ] = useState( true );
-
+    const [ color, setColor ] = useState( styles.disabled )
 
     const updateName = ( e ) => {
-        setName( e.target.value );
+        e.target.value = e.target.value.trim();
+        //the shortest pokemon name is 3 char and the longest is 11
         if ( e.target.value.length >= 3 && e.target.value.length < 12 ) {
+            setName( e.target.value );
             setIsDisabled( false );
+            setColor( styles.enabled )
+
         }
         else {
             setIsDisabled( true );
+            setColor(styles.disabled)
         }
-        
+
+
     }
 
     return (
         <>
-            <input type="text" placeholder='bulbasaur' onChange={ updateName }  />
-            <button onClick={ () => wasClicked( name ) } disabled={ isDisbled }>Search</button>
+            <input type="text" placeholder='bulbasaur' onChange={ updateName } />
+            <p>For Pokemons with a period in their name e.i Mr.Mime use a dash (-) instead.</p>
+            <button onClick={ () => wasClicked( name ) } disabled={ isDisbled } className={ color }>Search</button>
 
         </>
     )
