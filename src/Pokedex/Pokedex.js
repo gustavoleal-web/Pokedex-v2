069b8344be2f7b3cb2ed28e19d4c3746e5ddbcfa
gustavoleal-web <lealgustavo.web@gmail.com>
2020-12-18 11:37:32 -0600

    added the moreInfo comp and a spinner

--- src/Pokedex/Pokedex.js
+++ src/Pokedex/Pokedex.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import MoreInfo from './MoreInfo/MoreInfo'
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 import styles from './Pokedex.module.css';
+import { Spinner } from 'reactstrap';
 
 const Pokedex = ( { id } ) => {
+
     const [ state, setState ] = useState( [] );
 
     useEffect( () => {
         const fetchData = async () => {
             try {
-                let pokemon = await axios.get( `https://pokeapi.co/api/v2/pokemon/${id}` )
+                let pokemon = await axios.get( `https://pokeapi.co/api/v2/pokemon/${ id }` )
                 let pokemonEntries = pokemon.data;
                 setState( pokemonEntries );
             }
@@ -19,12 +22,14 @@ const Pokedex = ( { id } ) => {
         }
         fetchData();
 
-    }, [id] );
- 
+    }, [ id ] );
+
     return (
         <>
-            { Object.keys( state ).length === 0 ? <h1>Loading...</h1> :
+            { Object.keys( state ).length === 0 ? <Spinner color="primary" style={ { display: 'flex' } } /> :
+
                 <div className={ styles.infoContainer }>
+
                     <div>
                         <p>No.{ state.id }:  { state.name.toUpperCase() }</p>
                         <img src={ `${ state.sprites.front_default }` } alt={ state.name } />
@@ -33,6 +38,13 @@ const Pokedex = ( { id } ) => {
                             ? <p>{ state.types[ 0 ].type.name }, { state.types[ 1 ].type.name }</p>
                             : <p>{ state.types[ 0 ].type.name }</p>
                         }
+                        <MoreInfo
+                            name={ state.name }
+                            abilities={ state.abilities }
+                            sprites={ state.sprites }
+                            weight={ state.weight }
+                            height={ state.height }
+                        />
 
                     </div>
                     <div className={ styles.stats }>
