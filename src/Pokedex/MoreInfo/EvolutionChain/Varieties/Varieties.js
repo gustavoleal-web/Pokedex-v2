0fb1bf40bfc6ffec1aa1637bcfa1cff1b2c57534
gustavoleal-web <lealgustavo.web@gmail.com>
2021-01-09 09:12:26 -0600

    new compnt for different Poke varients

---
+++ src/Pokedex/MoreInfo/EvolutionChain/Varieties/Varieties.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Pokedex from '../../../Pokedex';
+
+const Varieties = ( { otherForms } ) => {
+    const [ form, setForms ] = useState( null );
+
+    const test = ( url ) => {
+        //extracted only the id from the url b/c using the Pokemon doesn't always work
+        let id = url.slice( 34, url.length - 1 );
+        //will have to add the prop clickedPoke b/c the other Pokedex has is
+        setForms( <Pokedex id={ id } /> )
+    }
+
+    return (
+        <div>
+            {
+                otherForms.map( form => {
+                    if ( form.is_default === false ) {
+                        return (
+                            <p key={ form.pokemon.name }
+                                onClick={ () => test( form.pokemon.url ) }>
+                                { form.pokemon.name }
+                            </p>
+                        )
+                    }
+                } ) }
+            {form }
+        </div>
+    )
+}
+
+export default React.memo( Varieties );
