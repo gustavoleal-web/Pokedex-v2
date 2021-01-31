231978a989f61dd112431ea38ad30c3da5213992
gustavoleal-web <lealgustavo.web@gmail.com>
2021-01-30 18:53:17 -0600

    replaced block of code with new comp that does the same

--- src/Pokedex/MoreInfo/EvolutionChain/EvolutionChain.js
+++ src/Pokedex/MoreInfo/EvolutionChain/EvolutionChain.js
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import setEvoChain from './setEvoChain';
 import filterEvolutionsTriggers from './filterEvolutionsTriggers';
 import EvosMethods from './EvosMethods';
 import axios from 'axios';
 
 const EvolutionChain = ( { evolutionChainUrl, clickedPoke } ) => {
-    const [ state, setState ] = useState( [] );
+    // const [ state, setState ] = useState( [] );
     const [ methods, setMethods ] = useState( [] );
 
     useEffect( () => {
         const fetchData = async () => {
-            let pokeEvoLine;
+
             let evosAndAlternative;
             try {
                 let pokemon = await axios.get( evolutionChainUrl );
                 let fetchedEvoChain = pokemon.data.chain;
 
-                pokeEvoLine = setEvoChain( fetchedEvoChain );
-                setState( pokeEvoLine );
+                // pokeEvoLine = setEvoChain( fetchedEvoChain );
+                // setState( pokeEvoLine );
+
 
                 //testing all evolutions and alternative evolutions
                 evosAndAlternative = filterEvolutionsTriggers( fetchedEvoChain );
                 setMethods( evosAndAlternative );
+              
             }
             catch ( e ) {
                 console.log( e );
@@ -32,22 +33,16 @@ const EvolutionChain = ( { evolutionChainUrl, clickedPoke } ) => {
 
     }, [ evolutionChainUrl ] );
 
-    return (
-        <div>
+    if ( methods.length === 0 ) {
+        return <p>This Pokemon does not evolve.</p>
+    }
 
-            {/*state.length === 0
-                ? <p>This Pokemon does not evolve.</p>
-                : state.map( pokemonEvo =>
-                    <p onClick={ () => clickedPoke( pokemonEvo ) }
-                        style={ { display: 'flex', color: 'blue' } }
-                        key={ pokemonEvo }>
-                        { pokemonEvo }
-                    </p> )
-                */ }
+    else {
+        return <EvosMethods methods={ methods } clickedPoke={ clickedPoke } />
+    }
 
-            <EvosMethods methods={ methods } clickedPoke={clickedPoke}/>
-        </div>
-    )
+
 }
+
 
 export default EvolutionChain;
