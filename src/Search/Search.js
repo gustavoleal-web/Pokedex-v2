f820f5779c8a6242ea35112a1c674250e8887080
gustavoleal-web <lealgustavo.web@gmail.com>
2021-02-04 17:18:42 -0600

    replace input and button with reactstrap elems

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styles from './search.module.css';
-
+import { Button, Input } from 'reactstrap';
+import icon from '../img/icons/loupe.png'
 
 
 //TODO: clear input after button is pressed
@@ -8,7 +9,6 @@ import styles from './search.module.css';
 const Search = ( { wasClicked } ) => {
     const [ name, setName ] = useState( '' )
     const [ isDisbled, setIsDisabled ] = useState( true );
-    const [ color, setColor ] = useState( styles.disabled )
 
     const updateName = ( e ) => {
         e.target.value = e.target.value.trim();
@@ -16,21 +16,24 @@ const Search = ( { wasClicked } ) => {
         if ( e.target.value.length >= 3 && e.target.value.length < 12 ) {
             setName( e.target.value );
             setIsDisabled( false );
-            setColor( styles.enabled )
-
         }
         else {
             setIsDisabled( true );
-            setColor( styles.disabled )
         }
 
     }
 
     return (
         <>
-            <div className={styles.container}>
-                <input type="text" placeholder='bulbasaur' onChange={ updateName } />
-                <button onClick={ () => wasClicked( name ) } disabled={ isDisbled } className={`${ color } ${styles.search}`}>Search</button>
+            <div className={ styles.container }>
+                <Input placeholder="Pokemon" onChange={ updateName } />
+                <Button
+                    onClick={ () => wasClicked( name ) }
+                    disabled={ isDisbled }
+                    color='secondary'
+                    className={ styles.button }>
+                    <img src={ icon } alt='search icon' className={ styles.searchIcon } />
+                </Button>
             </div>
 
             <p>For Pokemons with a period in their name e.i Mr.Mime use a dash (-) instead.</p>
