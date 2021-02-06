816f63a4521bddb3887e976b7cc5415fdcc60c86
gustavoleal-web <lealgustavo.web@gmail.com>
2021-02-05 18:26:24 -0600

    changed ModalFooter to ModalBody & added hr

--- src/Pokedex/MoreInfo/MoreInfo.js
+++ src/Pokedex/MoreInfo/MoreInfo.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Abilities from './Abilities/Abilities';
+import Training from './Training/Training';
 import EvolutionChain from './EvolutionChain/EvolutionChain';
 import AlternateForms from './AlternateForms/alternateForms';
 import Varieties from './Varieties/Varieties';
@@ -7,16 +8,15 @@ import ShinyPoke from './ShinyPoke/ShinyPoke';
 import styles from './moreInfo.module.css';
 import axios from 'axios';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-
-
-
+import filterDexEntries from './filterDexEntries';
 
 const MoreInfo = ( { name, abilities, sprites, weight, height, moves, pokeForms, clickedPoke, backgroundColor } ) => {
     const [ modal, setModal ] = useState( false );
-    const commonAbilities = [];
-    const hiddenAbilities = [];
     const [ evolutionChainUrl, setUrl ] = useState( '' );
     const [ varieties, setVarieties ] = useState( '' );
+    const [ trainingData, setTrainigData ] = useState( {} );
+    const commonAbilities = [];
+    const hiddenAbilities = [];
 
     //from reactstrap
     const toggle = () => setModal( !modal );
@@ -58,8 +58,6 @@ const MoreInfo = ( { name, abilities, sprites, weight, height, moves, pokeForms,
         let end = name.indexOf( '-' );
         name = name.substring( 0, end );
     }
-    ////////////////////
-    //Untitled goes HERE
 
     useEffect( () => {
         const fetchData = async () => {
@@ -67,6 +65,18 @@ const MoreInfo = ( { name, abilities, sprites, weight, height, moves, pokeForms,
 
                 let pokemon = await axios.get( `https://pokeapi.co/api/v2/pokemon-species/${ name }` );
                 let fetchedEvoChainURL = pokemon.data.evolution_chain.url;
+
+                let fetechedTraining = {};
+                ( {
+                    base_happiness: fetechedTraining.baseHappiness,
+                    capture_rate: fetechedTraining.captureRate,
+                    growth_rate: fetechedTraining.grouthRate
+                } = pokemon.data );
+                setTrainigData( fetechedTraining )
+
+
+
+                filterDexEntries( pokemon.data.flavor_text_entries )
                 setUrl( fetchedEvoChainURL );
 
                 pokemon.data.varieties ? setVarieties( pokemon.data.varieties ) : setVarieties( '' );
@@ -96,13 +106,14 @@ const MoreInfo = ( { name, abilities, sprites, weight, height, moves, pokeForms,
 
                 <ModalBody>
                     <div>
+                        <hr className={ styles.hrStyleOne } />
                         <h6>Common</h6>
                         <Abilities abilities={ commonAbilities } />
 
                         <h6>Hidden</h6>
                         <Abilities abilities={ hiddenAbilities } />
 
-                        <hr />
+
                         <div>
                             <p>Weight: { weightInKg } kg</p>
                             <p>Height: { feet } ft</p>
@@ -111,21 +122,33 @@ const MoreInfo = ( { name, abilities, sprites, weight, height, moves, pokeForms,
                     </div>
                 </ModalBody>
 
-                <h5>Evolution</h5>
-                <ModalFooter style={{display: 'block'}}>
+
+                <ModalBody>
+                    <hr className={ styles.hrStyleTwo } />
+                    { Object.keys( trainingData ).length !== 0 ? <Training data={ trainingData } /> : null }
+                </ModalBody>
+
+
+                <ModalBody style={ { display: 'block' } }>
+                    <hr className={ styles.hrStyleThree } />
                     { <EvolutionChain clickedPoke={ clickedPoke } evolutionChainUrl={ evolutionChainUrl } /> }
-                </ModalFooter>
+                </ModalBody>
 
-                <h5>Varieties</h5>
-                <ModalFooter>
+
+                <ModalBody>
+                    <hr className={ styles.hrStyleFour } />
                     { varieties.length === 0 ? null : <Varieties varieties={ varieties } clickedPoke={ clickedPoke } /> }
-                </ModalFooter>
+                </ModalBody>
 
-                <h5>Forms</h5>
-                <ModalFooter>
-                    { pokeForms.length !== 0 ? pokeForms.map( poke => <AlternateForms pokeForms={ poke } key={ poke.url } /> ) : null }
-                    <ShinyPoke shinySprite={ sprites.front_shiny } />
-                </ModalFooter>
+
+                <ModalBody>
+                    <hr className={ styles.hrStyleFive } />
+                    <div className={styles.formsContainer}>
+                        { pokeForms.length !== 0 ? pokeForms.map( poke => <AlternateForms pokeForms={ poke } key={ poke.url } /> ) : null }
+                        <ShinyPoke shinySprite={ sprites.front_shiny } />
+                    </div>
+
+                </ModalBody>
             </Modal>
         </div>
     );
