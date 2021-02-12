3a0abd921fc9220fb078186e2d654d17279f6c12
gustavoleal-web <lealgustavo.web@gmail.com>
2021-02-12 15:41:18 -0600

    added if check for missing egg data from api

--- src/Pokedex/MoreInfo/Breeding/Breeding.js
+++ src/Pokedex/MoreInfo/Breeding/Breeding.js
@@ -3,26 +3,32 @@ import React from 'react';
 const Breeding = ( { eggData } ) => {
     let groups;
     let stepsToHatch = eggData.hatchCounter * 250;
+    if ( eggData.eggGroups.length !== 0 ) {
+        if ( eggData.eggGroups[ 0 ].name === 'no-eggs' ) {
+            groups = <p>Egg Groups: Undiscovered</p>
+        }
 
-    if ( eggData.eggGroups[ 0 ].name === 'no-eggs' ) {
-        groups = <p>Egg Groups: Undiscovered</p>
+        else if ( eggData.eggGroups.length === 2 ) {
+            groups = <p>Egg Group: { eggData.eggGroups[ 0 ].name } and { eggData.eggGroups[ 1 ].name }</p>
+        }
+
+        else {
+            groups = <p>Egg Group: { eggData.eggGroups[ 0 ].name }</p>
+        }
+
+       
     }
-
-    else if ( eggData.eggGroups.length === 2 ) {
-        groups = <p>Egg Group: { eggData.eggGroups[ 0 ].name } and { eggData.eggGroups[ 1 ].name }</p>
-    }
-
     else {
-        groups = <p>Egg Group: { eggData.eggGroups[ 0 ].name }</p>
+        groups = null
     }
-
 
     return (
         <>
-            {groups}
-            <p>Hatch Counter: {eggData.hatchCounter}  (Approx: {stepsToHatch} steps)</p>
+            {groups }
+            <p>Hatch Counter: { eggData.hatchCounter }  (Approx: { stepsToHatch } steps)</p>
         </>
     )
 }
+
 
 export default Breeding
