e403539ea795d6d1c239f272982b1741e4205c48
gustavoleal-web <lealgustavo.web@gmail.com>
2021-02-11 20:36:53 -0600

    inclued artist contribution for the icons

--- src/App.js
+++ src/App.js
@@ -89,8 +89,13 @@ const App = () => {
         Icons made by <a href='https://www.freepik.com' title='Freepik'>Freepik
         </a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a>
       </div>
+      <div>
+        Icons made by <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar
+        </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      </div>
     </div>
   )
 
 }
+
 export default App;
