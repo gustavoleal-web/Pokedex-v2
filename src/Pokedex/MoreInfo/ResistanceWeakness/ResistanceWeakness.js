4f4dcde32421b973d240744f8c60709ed8aef17c
gustavoleal-web <lealgustavo.web@gmail.com>
2021-02-28 11:53:32 -0600

    new func for all resistances

--- src/Pokedex/MoreInfo/ResistanceWeakness/ResistanceWeakness.js
+++ src/Pokedex/MoreInfo/ResistanceWeakness/ResistanceWeakness.js
@@ -5,17 +5,11 @@ import { v4 as uuidv4 } from 'uuid';
 const ResistanceWeakness = ( { type } ) => {
     const [ state, setState ] = useState( [] );
 
-    const damage = {
-        double: [],
-        quadruple: [],
-        half: 0.5,
-        zero: 0
-    }
-
     useEffect( () => {
         const fetchData = async () => {
             if ( type.length === 1 ) {
                 try {
+
                     let pokemon = await axios.get( `${ type[ 0 ].type.url }` )
                     let arr = [ pokemon.data ]
                     setState( arr )
@@ -27,8 +21,12 @@ const ResistanceWeakness = ( { type } ) => {
             }
             else if ( type.length === 2 ) {
                 try {
-                    let promise1 = axios.get( `${ type[ 0 ].type.url }` );
-                    let promise2 = axios.get( `${ type[ 1 ].type.url }` );
+                    //the api can't fetched the url if it has a '/' at the end to it is removed
+                    const editedUrl1 = type[ 0 ].type.url.slice( 0, -1 )
+                    const editedUrl2 = type[ 1 ].type.url.slice( 0, -1 )
+
+                    let promise1 = axios.get( editedUrl1 );
+                    let promise2 = axios.get( editedUrl2 );
 
                     let p1 = await promise1;
                     let p2 = await promise2;
@@ -49,6 +47,10 @@ const ResistanceWeakness = ( { type } ) => {
     //vise versa for doubleDamege in 0 and halfDamage in 1
 
     const getAllWeakness = ( doubleDamage, halfDamage ) => {
+        let damage = {
+            normal: [],
+            weakness: []
+        }
         let double = [];
         let half = [];
 
@@ -59,16 +61,18 @@ const ResistanceWeakness = ( { type } ) => {
             half.push( halfDamage[ i ].name )
         }
 
-        let noDoublesWeakness = double.map( type => {
+        let noDoublesWeakness = []
+        double.forEach( type => {
             if ( half.includes( type ) ) {
-                return null
+                damage.normal.push( type )
             }
             else {
-                return type
+                noDoublesWeakness.push( type )
             }
         } );
 
-        return noDoublesWeakness;
+        damage.weakness = [ ...noDoublesWeakness ];
+        return damage;
     }
 
 
@@ -76,14 +80,37 @@ const ResistanceWeakness = ( { type } ) => {
         noDamage.map( type => {
             if ( noDuplicateWeaknesses.includes( type.name ) ) {
                 noDuplicateWeaknesses.splice( noDuplicateWeaknesses.indexOf( type.name ), 1 )
-
             }
             return true;
         } );
         return true;
     }
 
+    const oneFourthAndHalfDamage = ( typeOne, typeTwo ) => {
+        let fractionalDamage = {
+            fourth: [],
+            half: []
+        }
 
+        let arr1 = [];
+        let arr2 = [];
+        for ( let value of typeOne ) {
+            arr1.push( value.name )
+        }
+        for ( let value of typeTwo ) {
+            arr2.push( value.name )
+        }
+
+        let oneFourthDamage = arr1.filter( type => arr2.includes( type ) );
+        let oneHalfDamage1 = arr1.filter( type => !arr2.includes( type ) )
+        let oneHalfDamage2 = arr2.filter( type => !arr1.includes( type ) );
+        let oneHalfDamage = [ ...oneHalfDamage1, ...oneHalfDamage2 ];
+        fractionalDamage.fourth = [ ...oneFourthDamage ];
+        fractionalDamage.half = [ ...oneHalfDamage ];
+
+        return fractionalDamage;
+
+    }
 
     if ( state.length === 1 ) {
         return (
@@ -95,7 +122,6 @@ const ResistanceWeakness = ( { type } ) => {
                             <p key={ weakness.name }>{ weakness.name }</p> )
                     }
                 </div>
-
 
                 <h4>Resistance</h4>
                 <div>
@@ -111,6 +137,15 @@ const ResistanceWeakness = ( { type } ) => {
 
     }
     else if ( state.length === 2 ) {
+        let damage = {
+            oneHalf: [],
+            oneFourth: [],
+            normal: [],
+            double: [],
+            quadruple: []
+        }
+
+
         let doubleDamageTypeOne = state[ 0 ].damage_relations.double_damage_from;
         let halfDamageTypeTwo = state[ 1 ].damage_relations.half_damage_from;
 
@@ -128,8 +163,23 @@ const ResistanceWeakness = ( { type } ) => {
         let type1Weakness = getAllWeakness( doubleDamageTypeOne, halfDamageTypeTwo );
         let type2Weakness = getAllWeakness( doubleDamageTypeTwo, halfDamageTypeOne );
 
-        let allWeakNesses = [ ...type1Weakness, ...type2Weakness ];
+        let allWeakNesses = [ ...type1Weakness.weakness, ...type2Weakness.weakness ];
+        let test = {};
+
+
+        for ( let i = 0; i < allWeakNesses.length; i++ ) {
+
+            if ( test[ allWeakNesses[ i ] ] === undefined ) {
+                test[ allWeakNesses[ i ] ] = 0
+            }
+            else {
+                test[ allWeakNesses[ i ] ] += 1;
+            }
+        }
+        console.log( test )
+
         let noDuplicateWeaknesses = [ ...new Set( allWeakNesses ) ];
+
 
         //a dual type pokemon may have a type that is resistant to the other
         //removing it from the array so it doesn't include the resistant type as a weakness
@@ -139,38 +189,18 @@ const ResistanceWeakness = ( { type } ) => {
         removeResistanceFromWeakness( noDamageFromTypeTwo, noDuplicateWeaknesses );
         removeResistanceFromWeakness( noDamageFromTypeOne, noDuplicateWeaknesses );
 
-        const quadrupleDamage = type1Weakness.filter( value => type2Weakness.includes( value ) );
-        damage.quadruple = quadrupleDamage;
+        let frac = oneFourthAndHalfDamage( state[ 0 ].damage_relations.half_damage_from, state[ 1 ].damage_relations.half_damage_from );
 
-        //removes quadruple type from the weaknesses array and the remaining are the types that cause double damage
-        const doubleDamage = noDuplicateWeaknesses.filter( value => !quadrupleDamage.includes( value ) );
-        damage.double = doubleDamage;
-
-        let resistanceOne = state[ 0 ].damage_relations.half_damage_from.map( value => value.name );
-        let resistanceTwo = state[ 1 ].damage_relations.half_damage_from.map( value => value.name );
-
-        let oneFourth = resistanceOne.filter( value => resistanceTwo.includes( value ) );
-        let test = resistanceOne.filter( value => !resistanceTwo.includes( value ) )
-
-        resistanceOne.map( value => {
-            if ( resistanceTwo.indexOf( value ) !== -1 ) {
-                resistanceTwo.splice( resistanceTwo.indexOf( value ), 1 )
-            }
-        } );
-
-        let half = [...test, ...resistanceTwo];
-        console.log(half)
-
-
+        damage.normal = [ ...type1Weakness.normal, ...type1Weakness.normal ]
+        damage.oneHalf = [ ...frac.half ];
+        damage.oneFourth = [ ...frac.fourth ];
 
         return (
             <>
                 <h4>Weakness</h4>
                 { noDuplicateWeaknesses.map( weakness => <p key={ uuidv4() }>{ weakness }</p> ) }
             </>
-
         )
-
     }
     else {
         return (
