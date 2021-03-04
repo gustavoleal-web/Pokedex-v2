e7d44dc23f7688a46fcc42e2e0696e7d7b338ba0
gustavoleal-web <lealgustavo.web@gmail.com>
2021-03-04 12:35:06 -0600

    renamed the file

---
+++ src/Pokedex/MoreInfo/ResistanceWeakness/Damage/Damage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from '../ResistanceWeakness.module.css'
+import backgroundTypeColor from '../../../pokeTypes.module.css'
+
+const Damage = ( { damageList, timesDamage } ) => {
+    //if resist is an object it will execute this block
+    //it is needed b/c the prop type.name will need to be accessed unlike the arr in the else check
+    if ( damageList[ 0 ].hasOwnProperty( 'name' ) ) {
+        return (
+            <>
+                <h6>{ timesDamage }</h6>
+                <span className={ styles.container }>
+                    { damageList.map( type => <p className={ backgroundTypeColor[ type.name ] } key={ type.name }>{ type.name }</p> ) }
+
+                </span>
+            </>
+
+
+        )
+
+    }
+    else {
+        return (
+            <>
+                <h6>{ timesDamage }</h6>
+                <span className={ styles.container }>
+                    { damageList.map( type => <p className={ backgroundTypeColor[ type ] } key={ type }>{ type }</p> ) }
+                </span>
+            </>
+        )
+    }
+
+}
+
+export default Damage;
