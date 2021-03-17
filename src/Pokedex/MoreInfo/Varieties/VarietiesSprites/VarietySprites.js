8ac3640d2ec528aedad7c8f1106491a886737907
gustavoleal-web <lealgustavo.web@gmail.com>
2021-03-17 13:21:03 -0500

    poke variety sprite img now show up

---
+++ src/Pokedex/MoreInfo/Varieties/VarietiesSprites/VarietySprites.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const VarietySprites = ( { url } ) => {
+    const [ sprite, setSprites ] = useState( null );
+
+    useEffect( () => {
+        let isMounted = true;
+        const fetchData = async () => {
+            if ( isMounted ) {
+                try {
+                    let pokemon = await axios.get(
+                        url,
+                        { 'headers': { 'Access-Control-Allow-Origin': '*' } } );
+
+                    if ( pokemon.status === 200 ) {
+                        setSprites( pokemon.data.sprites.front_default )
+                    }
+
+
+                }
+                catch ( e ) {
+                    console.log( e )
+                }
+            }
+            return () => {
+                isMounted = false;
+            };
+
+        }
+        fetchData();
+
+    }, [ url ] );
+
+    if ( sprite !== null ) {
+        return <img src={ sprite } alt={sprite}/>
+    }
+    else return null;
+}
+
+export default VarietySprites;
