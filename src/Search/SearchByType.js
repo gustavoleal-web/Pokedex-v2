6f8cf9f8a5035d4aee142381ce7cede6da907848
gustavoleal-web <lealgustavo.web@gmail.com>
2021-03-16 19:29:39 -0500

    converted types to dropdown

--- src/Search/SearchByType.js
+++ src/Search/SearchByType.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 const SearchByType = ( { setPokedexByTypeHandler } ) => {
-    const types = [ 'fire', 'water', 'grass' ]
-    return <div>
+
+    const types = [ 'fire', 'water', 'grass',
+        'normal', 'electric', 'ice',
+        'fighting', 'poison', 'ground',
+        'flying', 'psychic', 'bug',
+        'rock', 'ghost', 'dark',
+        'dragon', 'steel', 'fairy' ]
+
+    return <DropdownButton id="dropdown-basic-button" title="Types" variant="light">
         {
-            types.map( type => <button onClick={ () => setPokedexByTypeHandler( type ) } key={type}>{ type }</button> )
+            types.map( type =>
+               
+                    <Dropdown.Item
+                        onClick={ () => setPokedexByTypeHandler( type ) }
+                        key={ type }>
+                        { type }
+                    </Dropdown.Item>
+                   
+
+            )
         }
-    </div>
+    </DropdownButton>
 }
 
 export default SearchByType;
