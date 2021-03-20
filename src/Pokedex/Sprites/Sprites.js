4423a10b5794d209dbc49d9092e6af619c92f7bd
gustavoleal-web <lealgustavo.web@gmail.com>
2021-03-20 13:45:52 -0500

    changed ternary to if else

--- src/Pokedex/Sprites/Sprites.js
+++ src/Pokedex/Sprites/Sprites.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import styles from './sprites.module.css'
 
-const Sprites = ( { pokeImg, name } ) => {
-    return (
-        
-        pokeImg.other[ "official-artwork" ].front_default !== null
-            ? <img style={ { width: '30%' } } src={ `${ pokeImg.other[ "official-artwork" ].front_default }` } alt={ name } />
-            : pokeImg.front_default !== null
-                //not all pokemon, especially the varieties have official artwork
-                //so sprites will be shown in place
-                //other wise it will display 'Image N/A'
-                ? <img src={ `${ pokeImg.front_default }` } alt={ name } />
-                : <p>Image: N/A</p>
-    )
+const Sprites = ( { pokeImg, name, showModalHandler } ) => {
+    //not all pokemon, especially the varieties have official artwork
+    //so sprites will be shown in place
+    //other wise it will display 'Image N/A'
+
+    if ( pokeImg.other[ 'official-artwork' ].front_default !== null ) {
+        return <img
+            className={ `${ styles.imgWidth } ${ styles.pointer }` }
+            src={ `${ pokeImg.other[ "official-artwork" ].front_default }` }
+            alt={ name }
+            onClick={ () => showModalHandler( true ) }
+        />
+    }
+
+    else if ( pokeImg.front_default !== null ) {
+        return <img
+            src={ `${ pokeImg.front_default }` }
+            alt={ name }
+            onClick={ () => showModalHandler( true ) }
+        />
+    }
+
+    else return <p onClick={ () => showModalHandler( true ) }>Image: N/A</p>
 }
 
 export default Sprites;
