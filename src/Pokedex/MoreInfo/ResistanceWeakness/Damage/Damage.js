e0b2b3f765a6f56a86f900a01588faf96b77c783
gustavoleal-web <lealgustavo.web@gmail.com>
2021-03-21 09:31:26 -0500

    added keys to span tag

--- src/Pokedex/MoreInfo/ResistanceWeakness/Damage/Damage.js
+++ src/Pokedex/MoreInfo/ResistanceWeakness/Damage/Damage.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import styles from '../ResistanceWeakness.module.css';
 import backgroundTypeColor from '../../../pokeTypes.module.css';
+import { v4 as uuidv4 } from 'uuid';
 
 
 const Damage = ( { damageList, timesDamage } ) => {
-
     //if resist is an object it will execute this block
     //it is needed b/c the prop type.name will need to be accessed unlike the arr in the else check
-    if ( damageList[ 0 ].hasOwnProperty( 'name' ) ) {
+    if ( damageList[ 0 ].hasOwnProperty( 'name' )) {
         return (
             <>
                 <h6>{ timesDamage }</h6>
-                <span className={ styles.container }>
+                <span className={ styles.container } key={uuidv4()}>
                     {
                         damageList.map( type =>
                             <p
@@ -32,7 +32,7 @@ const Damage = ( { damageList, timesDamage } ) => {
         return (
             <>
                 <h6>{ timesDamage }</h6>
-                <span className={ styles.container }>
+                <span className={ styles.container }  key={uuidv4()}>
                     { damageList.map( type => <p className={ backgroundTypeColor[ type ] } key={ type } >{ type }</p> ) }
 
                 </span>
