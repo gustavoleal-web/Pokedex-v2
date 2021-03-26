903e70f3a9fbbfb164e1619d847c3c114bdbba3b
gustavoleal-web <lealgustavo.web@gmail.com>
2021-03-26 12:10:06 -0500

    imported new comp

--- src/Pokedex/MoreInfo/Varieties/Varieties.js
+++ src/Pokedex/MoreInfo/Varieties/Varieties.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Pokedex from '../../Pokedex';
+import VarietiesSprites from './VarietiesSprites/VarietySprites'
+import styles from './varieties.module.css'
 
 const Varieties = ( { varieties, clickedPoke } ) => {
     const [ form, setForms ] = useState( null );
@@ -7,7 +9,7 @@ const Varieties = ( { varieties, clickedPoke } ) => {
     const retrieveById = ( url ) => {
         //extracted only the id from the url b/c using the Pokemon doesn't always work
         let id = url.slice( 34, url.length - 1 );
-        setForms( <Pokedex id={ id } clickedPoke={clickedPoke}/> )
+        setForms( <Pokedex id={ id } clickedPoke={ clickedPoke } /> )
     }
 
     return (
@@ -16,10 +18,15 @@ const Varieties = ( { varieties, clickedPoke } ) => {
                 varieties.map( form => {
                     if ( form.is_default === false ) {
                         return (
-                            <p key={ form.pokemon.name }
-                                onClick={ () => retrieveById( form.pokemon.url ) }>
-                                { form.pokemon.name }
-                            </p>
+                            <span key={ form.pokemon.name }>
+                                <p
+                                    onClick={ () => retrieveById( form.pokemon.url ) }
+                                    className={ styles.cursor } >
+                                    { form.pokemon.name }
+                                </p>
+                                <VarietiesSprites url={form.pokemon.url}/>
+                            </span>
+
                         )
                     }
                     return true;
