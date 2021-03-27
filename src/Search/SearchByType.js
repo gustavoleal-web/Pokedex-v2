8ef098f5979475f955d05b8f4bc966234208677a
gustavoleal-web <lealgustavo.web@gmail.com>
2021-03-27 11:59:42 -0500

    changed double to single quotes

--- src/Search/SearchByType.js
+++ src/Search/SearchByType.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import styles from '../SelectedPokedex/selectPokedex.module.css'
 
 const SearchByType = ( { setPokedexByTypeHandler } ) => {
 
@@ -11,16 +12,21 @@ const SearchByType = ( { setPokedexByTypeHandler } ) => {
         'rock', 'ghost', 'dark',
         'dragon', 'steel', 'fairy' ]
 
-    return <DropdownButton id="dropdown-basic-button" title="Types" variant="light">
+    return <DropdownButton
+        id='dropdown-basic-button'
+        title='Types'
+        variant='light'
+        className={ styles.searchDropDown }>
+        
         {
             types.map( type =>
-               
-                    <Dropdown.Item
-                        onClick={ () => setPokedexByTypeHandler( type ) }
-                        key={ type }>
-                        { type }
-                    </Dropdown.Item>
-                   
+
+                <Dropdown.Item
+                    onClick={ () => setPokedexByTypeHandler( type ) }
+                    key={ type }>
+                    { type }
+                </Dropdown.Item>
+
 
             )
         }
