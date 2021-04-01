e7b7250b764f3515f39cdb50aa1080fd6fd0e576
gustavoleal-web <lealgustavo.web@gmail.com>
2021-04-01 11:22:42 -0500

    changed button to dropdown

--- src/Dropdowns.js
+++ src/Dropdowns.js
@@ -1,24 +1,24 @@
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
+// import DropdownButton from 'react-bootstrap/DropdownButton';
+// import Dropdown from 'react-bootstrap/Dropdown';
+// import Navbar from 'react-bootstrap/NavBar'
+// import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
-const Dropdowns = ( { arr, title } ) => {
-    return <DropdownButton
-        id='dropdown-basic-button'
-        title={ title }
-        drop='right'
-        variant='secondary'
-    >
-        {
-            arr.map( element =>
-
-                <Dropdown.Item
-                    key={ element }>
+const Dropdowns = ( { arr, title, func } ) => {
+    return <NavDropdown title={title} id='collasible-nav-dropdown'>
+    {
+        arr.map( element =>
+            <span key={ element }>
+                <NavDropdown.Item
+                    onClick={ () =>  func( element ) }
+                >
                     { element }
 
-                </Dropdown.Item>
-            )
-        }
-    </DropdownButton>
+                </NavDropdown.Item>
+            </span>
+        )
+    }
+</NavDropdown>
 }
 
 export default Dropdowns;
