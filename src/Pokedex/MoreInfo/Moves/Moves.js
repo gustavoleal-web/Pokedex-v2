7b06279ad9f565b85661562fe19e1ef677241dfa
gustavoleal-web <lealgustavo.web@gmail.com>
2021-04-29 21:13:53 -0500

    new func to search for non empty list of moves

--- src/Pokedex/MoreInfo/Moves/Moves.js
+++ src/Pokedex/MoreInfo/Moves/Moves.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Moves = ( { moves } ) => {
+    const games = [ 'ultra-sun-ultra-moon', 'sun-moon', 'x-y', 'omega-ruby-alpha-sapphire' ];
 
     //function found online
     const compare = ( a, b ) => {
@@ -20,33 +21,58 @@ const Moves = ( { moves } ) => {
     const movesByLearnedMethod = ( method ) => {
         const levelUpMoves = moves.filter( move => {
             let version = move.version_group_details;
-            return version[ version.length - 1 ].move_learn_method.name === method;
+
+            if ( version.length === 1 && version[ version.length - 1 ].move_learn_method.name !== method ) {
+                return false;
+            }
+
+            else if ( version[ version.length - 1 ].move_learn_method.name === method ) {
+                return version[ version.length - 1 ];
+            }
+            else if ( version[ version.length - 2 ].move_learn_method.name === method ) {
+                return version[ version.length - 2 ];
+            }
+            else return false;
         } );
 
         return levelUpMoves
     }
 
     const movesByGameVersion = ( arr, pokemonGame ) => {
+        const movesByGame = []
         //make a separate array instead
         //push the moves into the erray only if the match the game
         //return the array
         //this way the value False doesn't get pushed to the array
-        const movesByGame = arr.map( move => {
+        arr.map( move => {
             let version = move.version_group_details;
             if ( version[ version.length - 1 ].version_group.name === pokemonGame ) {
-                return {
-                    name: move.move.name,
-                    level: version[ version.length - 1 ].level_learned_at
-                }
+                movesByGame.push(
+                    {
+                        name: move.move.name,
+                        level: version[ version.length - 1 ].level_learned_at
+                    }
+                )
 
             }
-            else {
-                return false;
-            };
-
+            return true;
         } );
 
         return movesByGame;
+    }
+
+    //func will search through games until it finds a non empty list of moves
+    const latestAvailableMoves = ( arr, movesArr ) => {
+        let j = 0
+        while ( arr.length === 0 ) {
+            arr = movesByGameVersion( movesArr, games[ j ] );
+
+            j++;
+            if ( j > games.length ) {
+                break;
+            }
+        }
+        return arr;
     }
 
 
@@ -54,11 +80,25 @@ const Moves = ( { moves } ) => {
     const levelUpMovesByGame = movesByGameVersion( levelUpMoves, 'ultra-sun-ultra-moon' );
 
     const tutorMoves = movesByLearnedMethod( 'tutor' );
-    const tutorMovesByGame = movesByGameVersion( tutorMoves, 'omega-ruby-alpha-sapphire' );
+    let tutorMovesByGame = movesByGameVersion( tutorMoves, 'ultra-sun-ultra-moon' );
+
+    if ( tutorMovesByGame.length === 0 ) {
+        tutorMovesByGame = latestAvailableMoves( tutorMovesByGame, tutorMoves );
+    }
 
     const tmMoves = movesByLearnedMethod( 'machine' );
+    let tmMovesByGame = movesByGameVersion( tmMoves, 'ultra-sun-ultra-moon' );
+
+    if ( tmMovesByGame.length === 0 ) {
+        tmMovesByGame = latestAvailableMoves( tmMovesByGame, tmMoves );
+    }
+
     const eggMoves = movesByLearnedMethod( 'egg' );
-    
+    let eggMovesByGame = movesByGameVersion( eggMoves, 'ultra-sun-ultra-moon' );
+
+    if ( eggMovesByGame.length === 0 ) {
+        eggMovesByGame = latestAvailableMoves( eggMovesByGame, eggMoves );
+    }
 
     levelUpMovesByGame.sort( compare );
 
@@ -68,3 +108,6 @@ const Moves = ( { moves } ) => {
 }
 
 export default Moves
+
+//filter through all the moves and return the matching game first
+//then filter by method next
