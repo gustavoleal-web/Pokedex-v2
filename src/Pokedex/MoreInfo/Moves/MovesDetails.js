71c1b88723d6b13da74c3a78602121e6990b711f
gustavoleal-web <lealgustavo.web@gmail.com>
2021-05-05 19:53:27 -0500

    added img to display move seals

--- src/Pokedex/MoreInfo/Moves/MovesDetails.js
+++ src/Pokedex/MoreInfo/Moves/MovesDetails.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styles from './MoveDetails.module.css';
+import backgroundColor from '../../pokeTypes.module.css'
 
 
-const MoveDetails = ( { name, level } ) => {
-    const [ allMoveInfo, setAllMoveInfo ] = useState( {} )
+const MoveDetails = ( { name, level, learnMethod } ) => {
+    const [ allMoveInfo, setAllMoveInfo ] = useState( {} );
+    const [ tm, setTm ] = useState( [] )
 
     useEffect( () => {
         let isMounted = true;
@@ -15,7 +17,7 @@ const MoveDetails = ( { name, level } ) => {
                         `https://pokeapi.co/api/v2/move/${ name }/` );
 
                     if ( pokemon.status === 200 ) {
-                        console.log( pokemon.data );
+                        //console.log( pokemon.data );
                         setAllMoveInfo( pokemon.data )
                     }
 
@@ -35,20 +37,68 @@ const MoveDetails = ( { name, level } ) => {
     }, [ name ] );
 
 
+    useEffect( () => {
+        let isMounted = true;
+        const fetchData = async () => {
+            if ( isMounted && Object.keys( allMoveInfo ).length !== 0 && learnMethod === 'machine' ) {
+
+                try {
+                    let url = allMoveInfo.machines[ allMoveInfo.machines.length - 1 ].machine.url;
+                    let pokemon = await axios.get( url );
+
+                    if ( pokemon.status === 200 ) {
+                        setTm( pokemon.data )
+                    }
+
+                }
+                catch ( e ) {
+                    console.log( e )
+                }
+            }
+            return () => {
+                isMounted = false;
+            };
+
+        }
+        fetchData();
+
+    }, [ allMoveInfo, learnMethod ] );
+
+    let levelOrTm = null;
+    if ( Object.keys( allMoveInfo ).length !== 0 && Object.keys( tm ).length !== 0 ) {
+        levelOrTm = <p className={ styles.start }>{ tm.item.name }</p>
+    }
+    else {
+        levelOrTm = <p className={ styles.start }> { level }</p>
+    }
+
+
     if ( Object.keys( allMoveInfo ).length !== 0 ) {
+        let type = `${ allMoveInfo.type.name }.png`;
+        let seals = `move-${ allMoveInfo.damage_class.name }.png`;
+        let movePower = allMoveInfo.power === null ? <p className={ styles.end }>-</p> : <p className={ styles.end }>{ allMoveInfo.power }</p>
+        let accuracy = allMoveInfo.accuracy === null ? <p className={ styles.end }>-</p> : <p className={ styles.end }>{ allMoveInfo.accuracy }</p>
 
         return (
             <>
                 <hr />
                 <div className={ styles.container }>
-                    <p className={ styles.start }> { level }</p>
+                    { levelOrTm }
                     <p className={ styles.start }>{ name }</p>
-                    <p className={ styles.start }>{ allMoveInfo.type.name }</p>
-                    <p className={ styles.start }> { allMoveInfo.damage_class.name }</p>
-                    { allMoveInfo.power === null ? <p className={ styles.end }>-</p> : <p className={ styles.end }>{ allMoveInfo.power }</p> }
-                    { allMoveInfo.accuracy === null ? <p className={ styles.end }>-</p> : <p className={ styles.end }>{ allMoveInfo.accuracy }</p> }
+                    <img
+                        src={ ` https://raw.githubusercontent.com/msikma/pokesprite/master/misc/types/go/${ type }` }
+                        alt={ allMoveInfo.type.name }
+                        className={styles.moveIcons}
+                    />
 
-                   
+                    <img
+                        src={ `https://raw.githubusercontent.com/msikma/pokesprite/master/misc/seals/home/${ seals }` }
+                        alt={ allMoveInfo.damage_class.name }
+                        className={styles.moveIcons}
+                    />
+
+                    { movePower }
+                    { accuracy }
                 </div>
 
             </> )
