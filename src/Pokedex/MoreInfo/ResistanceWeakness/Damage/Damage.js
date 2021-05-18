8966ee5cd09602a15f79dd4a56739d8a953565b7
gustavoleal-web <lealgustavo.web@gmail.com>
2021-05-17 19:32:12 -0500

    gave p and h tag classes

--- src/Pokedex/MoreInfo/ResistanceWeakness/Damage/Damage.js
+++ src/Pokedex/MoreInfo/ResistanceWeakness/Damage/Damage.js
@@ -7,16 +7,17 @@ import { v4 as uuidv4 } from 'uuid';
 const Damage = ( { damageList, timesDamage } ) => {
     //if resist is an object it will execute this block
     //it is needed b/c the prop type.name will need to be accessed unlike the arr in the else check
-    if ( damageList[ 0 ].hasOwnProperty( 'name' )) {
+    if ( damageList[ 0 ].hasOwnProperty( 'name' ) ) {
         return (
             <>
-                <h6>{ timesDamage }</h6>
-                <span className={ styles.container } key={uuidv4()}>
+                <h6 className={ styles.title }>{ timesDamage }</h6>
+                <span className={ styles.container } key={ uuidv4() }>
                     {
                         damageList.map( type =>
                             <p
                                 className={ backgroundTypeColor[ type.name ] }
-                                key={ type.name } >
+                                key={ type.name }
+                                style={ { fontSize: '18px' } }>
                                 { type.name }
                             </p> )
                     }
@@ -31,8 +32,8 @@ const Damage = ( { damageList, timesDamage } ) => {
     else {
         return (
             <>
-                <h6>{ timesDamage }</h6>
-                <span className={ styles.container }  key={uuidv4()}>
+                <h6 className={ styles.title }>{ timesDamage }</h6>
+                <span className={ styles.container } key={ uuidv4() }>
                     { damageList.map( type => <p className={ backgroundTypeColor[ type ] } key={ type } >{ type }</p> ) }
 
                 </span>
