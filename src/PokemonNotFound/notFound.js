338b11db5e3c1730ba657f4c61519d15dd5bf27b
gustavoleal-web <lealgustavo.web@gmail.com>
2021-05-20 20:53:09 -0500

    added Search comp to the not found comp

--- src/PokemonNotFound/notFound.js
+++ src/PokemonNotFound/notFound.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import Search from '../Search/Search'
 import notFoundImg from '../img/surprised_pikachu.jpg'
 import styles from './notFound.module.css'
 
 const notFound = () => {
     return (
-        <div className={styles.container}>
-            <img src={ notFoundImg } alt='No pokemon found.' className={ styles.border } />
-            <div>
-                <h1> Oops!</h1>
-                <h3>NO POKEMON FOUND</h3>
+        <>
+            <div className={styles.search}>
+                <Search />
             </div>
 
-        </div>
 
+            <div className={ styles.container }>
+                <img src={ notFoundImg } alt='No pokemon found.' className={ styles.border } />
+                <div>
+                    <h1> Oops!</h1>
+                    <h3>NO POKEMON FOUND</h3>
+                </div>
+
+            </div>
+
+        </>
     )
 
 }
