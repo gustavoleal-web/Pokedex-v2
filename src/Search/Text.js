227123663dcfbea42fdeb21cd1d9c7561550c056
gustavoleal-web <lealgustavo.web@gmail.com>
2021-06-03 10:45:12 -0500

    initial file commit

---
+++ src/Search/Text.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Search.module.css'
+
+const Text = ( { userInputSearch, highlightedAutoComplete, onClickHandler } ) => {
+    let value = `${ userInputSearch }${ highlightedAutoComplete }`
+    return (
+        <li onClick={ () => onClickHandler( value ) } className={ styles.listItem } >
+            <p>{ userInputSearch }</p>
+            <b>{ highlightedAutoComplete }</b>
+        </li>
+    )
+}
+
+export default Text;
