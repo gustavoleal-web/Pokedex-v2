de154af093561db327cc2298d6554503b74a941a
gustavoleal-web <lealgustavo.web@gmail.com>
2021-07-06 11:21:51 -0500

    a pokeball img will show when pokemon img fails to load

--- src/Pokedex/Sprites/Sprites.js
+++ src/Pokedex/Sprites/Sprites.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './sprites.module.css'
+import pokeballImg from '../../img/icons/pokeball.png'
 
 const Sprites = ( { pokeImg, name, showModalHandler } ) => {
     //not all pokemon, especially the varieties have official artwork
     //so sprites will be shown in place
     //other wise it will display 'Image N/A'
-
     if ( pokeImg.other[ 'official-artwork' ].front_default !== null ) {
         return <img
             className={ `${ styles.imgWidth } ${ styles.pointer }` }
@@ -24,7 +24,14 @@ const Sprites = ( { pokeImg, name, showModalHandler } ) => {
         />
     }
 
-    else return <p onClick={ () => showModalHandler( true ) }>Image: N/A</p>
+    else {
+        return <img
+            className={ `${ styles.pokeball } ${ styles.pointer }` }
+            src={ pokeballImg }
+            alt={ name }
+            onClick={ () => showModalHandler( true ) }
+        />
+    }
 }
 
 export default Sprites;
