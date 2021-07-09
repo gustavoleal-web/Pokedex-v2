a74875e1a9e29bff577b64cad34da6bee02af3e3
gustavoleal-web <lealgustavo.web@gmail.com>
2021-07-09 15:26:10 -0500

    added contribution to the sprite author

--- src/SelectedDropdownOption/SelectedDropdownOption.js
+++ src/SelectedDropdownOption/SelectedDropdownOption.js
@@ -82,6 +82,11 @@ const SelectedSearchOption = ( {
         : null
       }
 
+      <div>
+        Icons made by <a href='https://www.flaticon.com/authors/darius-dan' title='Darius Dan'>Darius Dan
+        </a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a>
+      </div>
+
     </div>
   )
 
